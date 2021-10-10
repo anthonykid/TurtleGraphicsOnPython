6f12f14680d92c8e7405f39591be96eede89c950
silver-spar <73019773+silver-spar@users.noreply.github.com>
2021-10-10 14:14:33 +0530

    I gave it a new look
    I have changed the background color, increased the pen size to 5, increased the speed 0 to 30 the ring color magenta to pink and I changed the starting side left to right

--- PythonTurtleGraphics.py
+++ PythonTurtleGraphics.py
@@ -2,16 +2,16 @@ import turtle
 
 t = turtle.Turtle()
 
-turtle.bgcolor("black")
-turtle.pensize(2)
-turtle.speed(0)
+turtle.bgcolor("navy")
+turtle.pensize(5)
+turtle.speed(30)
 
 while (True):
     for i in range(6):
-        for colors in["red", "blue", "magenta", "green", "yellow", "white"]:
+        for colors in["red", "blue", "pink", "green", "yellow", "white"]:
             turtle.color(colors)
             turtle.circle(100)
-            turtle.left(10)
+            turtle.right(10)
 
         turtle.hideturtle()
     turtle.mainloop()
